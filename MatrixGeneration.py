bd64d2842bce0941c6cc46928f2b6dd197970d9e
sebub <ssma22@student.aau.dk>
2023-05-03 16:05:59 +0200

    added data

--- MatrixGeneration.py
+++ MatrixGeneration.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-size = 100
+size = 3000
 arr = np.random.randint(0,10,(size,size))
-np.savetxt('matrixA100.csv',arr, fmt = '%d', delimiter=",")
+np.savetxt('matrixA3k.csv',arr, fmt = '%d', delimiter=",")
 arr2 = np.random.randint(0,10,(size,size))
-np.savetxt('matrixB100.csv',arr2, fmt = '%d', delimiter=",")
+np.savetxt('matrixB3k.csv',arr2, fmt = '%d', delimiter=",")
